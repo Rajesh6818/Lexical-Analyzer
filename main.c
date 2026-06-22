c52fd39e95fe1f0f059f90049272329eb009deaa
Rajesh6818 <rajeshyandamuri80@gmail.com>
2026-06-22 21:17:56 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "lexer.h"
+#include "read.c"
+
+int main(int argc,char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Invalid Arguments\n");
+        return 1;
+    }
+    read_fun(argv[1]);
+}
